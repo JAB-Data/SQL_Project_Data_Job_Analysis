9aa48c414f01b582716d6172fe471771884e90a2
Joseph Bernard <jabtor999@gmail.com>
2025-09-13 15:59:43 -0400

    Solution results for query 5

--- project_sql/5_optimal_skills.sql
+++ project_sql/5_optimal_skills.sql
@@ -30,6 +30,7 @@ top_paying_skills AS
 (
     SELECT 
         skills_dim.skill_id,
+        -- skills_dim.skills,
         ROUND(AVG(salary_year_avg), 0) AS avg_salary
     FROM 
         job_postings_fact
@@ -59,8 +60,34 @@ ORDER BY
     avg_salary DESC
 LIMIT 25;
 
+/*
+SALARY DEFINITIONS
+Category	    Percentile Range	Salary Range 	    Description
+High Salary	    75% – 100%	        ≥ $110,000	        Top quarter of salaries
+Medium Salary	25% – 75%	        $98,000 – $110,000	Middle half of salaries
+Low Salary	    0% – 25%	        ≤ $98,000	        Bottom quarter of salaries
 
+Q1 (bottom 25th percentile): ≈ $98,000
+Median (Q2): ≈ $102,204
+Mean (Average): ≈ $103,200
+Min: $86,163
+*
 
+INSIGHTS & TAKEAWAYS
+1. Python, Tableau and R
+These three skills strike the best balance between high demand and high salary.
+•	Python is the most versatile, useful for everything from data wrangling to automation and machine learning.
+•	Tableau dominates in data visualization—essential for communicating insights.
+•	R is especially valuable for statistical analysis and research-heavy roles.
+________________________________________
+💼 2. Popularity ≠ Pay: Choose Skills Strategically
+•	Tools like SQL and Excel are in high demand, but they offer lower average salaries.
+•	Skills like Go, Snowflake, and BigQuery offer high salaries, but are in lower demand—more niche.
+________________________________________
+🚀 3. Python, Tableau and R
+The skills that stand out (Python, Tableau, R) are not only currently in demand, 
+but are core to emerging trends in data science and analytics.
+*/
 
 
    
